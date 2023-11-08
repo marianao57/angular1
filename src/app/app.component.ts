1e8aeb7533e43c445768b24a718a815ea3e2751b
marianao57 <mariana.ortiz@pragma.com.co>
2023-11-08 09:26:22 -0500

    utilización de NgOptimizedImage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './models/product.model';
+//este paquete es para optimizar las imagenes, que a medida que el usuario va bajando las
+//imagenes van cargando, asi no cargan las 200 de una, sino hasta donde el usuario quiera ver
+//tambien se debe incorporar en app.module.ts
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   //el selector es la manera en que vamos a llamar el componente en html
@@ -21,10 +25,11 @@ export class AppComponent {
   //Este es un disparador, en este caso se va a consumir una api
   //No es una promesa, se utiliza rxjs, no hay promise por lo cual se usa .suscribe
   ngOnInit() {
-    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
       //en resp se tiene el json de la api
       .subscribe((data) => {
-        this.products = data
+        this.products = data;
       });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 //Para conectarse al protocolo http y se importa en la linea 12
 import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, ProductComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgOptimizedImage,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
